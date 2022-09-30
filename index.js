9d85346c0a3a5ae01fd20e1078b5a74682409dc4
sawfiz <dashu.up@gmail.com>
2022-09-30 11:05:42 +0800

    Add border to active button

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const btnRainbowEl = document.querySelector("#btn-rainbow");
 const btnEraseEl = document.querySelector("#btn-erase");
 const btnClearEl = document.querySelector("#btn-clear");
 const pointsEl = document.querySelector("#points");
-const colorPickerEl = document.querySelector("#color-picker")
+const colorPickerEl = document.querySelector("#color-picker");
 
 function makeGrid(dim) {
     gridContainerEl.style.cssText = `grid-template-columns: repeat(${dim}, 1fr); 
@@ -67,25 +67,42 @@ function emptyGrid() {
     gridContainerEl.innerHTML = "";
 }
 
+function removeActive() {
+    btnEraseEl.classList.remove("active")
+    btnColorEl.classList.remove("active")
+    btnRainbowEl.classList.remove("active")
+}
+
 function activateButtons() {
+    btnColorEl.classList.add("active");
     btnColorEl.addEventListener("click", () => {
         mode = "black";
         color = "#000000";
+        removeActive();
+        btnColorEl.classList.add("active");
     });
-    colorPickerEl.addEventListener("input", e => {
+    colorPickerEl.addEventListener("input", (e) => {
         mode = "personal";
         color = e.target.value;
         btnColorEl.innerText = `Current Color: ${color}`;
+        removeActive();
+        btnColorEl.classList.add("active");
         console.log(color);
-    })
+    });
     btnRainbowEl.addEventListener("click", () => {
         mode = "rainbow";
+        removeActive();
+        btnRainbowEl.classList.add("active");
     });
     btnEraseEl.addEventListener("click", () => {
         mode = "erase";
         color = "#ffffff";
+        removeActive();
+        btnEraseEl.classList.add("active");
     });
-    btnClearEl.addEventListener("click", clearGrid);
+    btnClearEl.addEventListener("click", () => {
+        clearGrid();
+    });
 
     pointsEl.addEventListener("input", makeNewGrid);
 }
@@ -94,4 +111,4 @@ function activateButtons() {
 window.onload = () => {
     makeGrid(DEFAULT_SIZE);
     activateButtons();
-  }
+};
