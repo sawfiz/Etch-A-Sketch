63db31fd29dbe87ecdeb409011aa7547f511bd2f
sawfiz <dashu.up@gmail.com>
2022-10-07 20:37:45 +0800

    Lint the js code

--- index.js
+++ index.js
@@ -1,115 +1,117 @@
 const DEFAULT_SIZE = 45;
 let drawing = false;
-let mode = "black";
-let color = "#000000";
+let mode = 'black';
+let color = '#000000';
 let currentSize = DEFAULT_SIZE;
 
-const gridContainerEl = document.querySelector(".grid-container");
-const btnColorEl = document.querySelector("#btn-color");
-const btnRainbowEl = document.querySelector("#btn-rainbow");
-const btnEraseEl = document.querySelector("#btn-erase");
-const btnClearEl = document.querySelector("#btn-clear");
-const pointsEl = document.querySelector("#points");
-const labelEl = document.querySelector("label");
-const colorPickerEl = document.querySelector("#color-picker");
+const gridContainerEl = document.querySelector('.grid-container');
+const btnColorEl = document.querySelector('#btn-color');
+const btnRainbowEl = document.querySelector('#btn-rainbow');
+const btnEraseEl = document.querySelector('#btn-erase');
+const btnClearEl = document.querySelector('#btn-clear');
+const pointsEl = document.querySelector('#points');
+const labelEl = document.querySelector('label');
+const colorPickerEl = document.querySelector('#color-picker');
 
-function makeGrid(dim) {
-    gridContainerEl.style.cssText = `grid-template-columns: repeat(${dim}, 1fr); 
-        grid-template-rows: repeat(${dim}, 1fr)`;
-
-    for (let row = 0; row < dim; row++) {
-        for (let col = 0; col < dim; col++) {
-            let dotEl = document.createElement("item");
-            gridContainerEl.appendChild(dotEl);
-            dotEl.addEventListener("mouseover", drawStuff);
-            dotEl.addEventListener("mousedown", () => {
-                drawing = true;
-                changeColor(dotEl);
-            });
-            dotEl.addEventListener("mouseup", () => {
-                drawing = false;
-            });
-        }
-    }
-}
-
-function drawStuff(e) {
-    if (drawing === true) {
-        changeColor(this);
-    }
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
 function changeColor(dot) {
-    if (mode === "rainbow") {
-        color = getRandomColor();
+  if (mode === 'rainbow') {
+    color = getRandomColor();
+  }
+  dot.style.backgroundColor = color;
+}
+
+function drawStuff() {
+  if (drawing === true) {
+    changeColor(this);
+  }
+}
+
+function makeGrid(dim) {
+  gridContainerEl.style.cssText = `grid-template-columns: repeat(${dim}, 1fr); 
+        grid-template-rows: repeat(${dim}, 1fr)`;
+
+  // eslint-disable-next-line no-plusplus
+  for (let row = 0; row < dim; row++) {
+    // eslint-disable-next-line no-plusplus
+    for (let col = 0; col < dim; col++) {
+      const dotEl = document.createElement('item');
+      gridContainerEl.appendChild(dotEl);
+      dotEl.addEventListener('mouseover', drawStuff);
+      dotEl.addEventListener('mousedown', () => {
+        drawing = true;
+        changeColor(dotEl);
+      });
+      dotEl.addEventListener('mouseup', () => {
+        drawing = false;
+      });
     }
-    dot.style.backgroundColor = color;
-}
-
-function getRandomColor() {
-    return "#" + Math.floor(Math.random() * 16777215).toString(16);
-}
-
-function clearGrid() {
-    emptyGrid();
-    makeGrid(currentSize);
-}
-
-function makeNewGrid(dim) {
-    emptyGrid();
-    dotArray = [];
-    makeGrid(dim);
-    currentSize = dim;
+  }
 }
 
 function emptyGrid() {
-    gridContainerEl.innerHTML = "";
+  gridContainerEl.innerHTML = '';
+}
+
+function clearGrid() {
+  emptyGrid();
+  makeGrid(currentSize);
+}
+
+function makeNewGrid(dim) {
+  emptyGrid();
+  //   dotArray = [];
+  makeGrid(dim);
+  currentSize = dim;
 }
 
 function removeActive() {
-    btnEraseEl.classList.remove("active")
-    btnColorEl.classList.remove("active")
-    btnRainbowEl.classList.remove("active")
+  btnEraseEl.classList.remove('active');
+  btnColorEl.classList.remove('active');
+  btnRainbowEl.classList.remove('active');
 }
 
 function activateButtons() {
-    btnColorEl.classList.add("active");
-    btnColorEl.addEventListener("click", () => {
-        mode = "black";
-        color = "#000000";
-        removeActive();
-        btnColorEl.classList.add("active");
-    });
-    colorPickerEl.addEventListener("input", (e) => {
-        mode = "personal";
-        color = e.target.value;
-        btnColorEl.innerText = `Current Color: ${color}`;
-        removeActive();
-        btnColorEl.classList.add("active");
-    });
-    btnRainbowEl.addEventListener("click", () => {
-        mode = "rainbow";
-        removeActive();
-        btnRainbowEl.classList.add("active");
-    });
-    btnEraseEl.addEventListener("click", () => {
-        mode = "erase";
-        color = "#ffffff";
-        removeActive();
-        btnEraseEl.classList.add("active");
-    });
-    btnClearEl.addEventListener("click", () => {
-        clearGrid();
-    });
+  btnColorEl.classList.add('active');
+  btnColorEl.addEventListener('click', () => {
+    mode = 'black';
+    color = '#000000';
+    removeActive();
+    btnColorEl.classList.add('active');
+  });
+  colorPickerEl.addEventListener('input', (e) => {
+    mode = 'personal';
+    color = e.target.value;
+    btnColorEl.innerText = `Current Color: ${color}`;
+    removeActive();
+    btnColorEl.classList.add('active');
+  });
+  btnRainbowEl.addEventListener('click', () => {
+    mode = 'rainbow';
+    removeActive();
+    btnRainbowEl.classList.add('active');
+  });
+  btnEraseEl.addEventListener('click', () => {
+    mode = 'erase';
+    color = '#ffffff';
+    removeActive();
+    btnEraseEl.classList.add('active');
+  });
+  btnClearEl.addEventListener('click', () => {
+    clearGrid();
+  });
 
-    pointsEl.addEventListener("input", () => {
-        labelEl.innerText = `${pointsEl.value} x ${pointsEl.value} `;
-        makeNewGrid(pointsEl.value);
-    });
+  pointsEl.addEventListener('input', () => {
+    labelEl.innerText = `${pointsEl.value} x ${pointsEl.value} `;
+    makeNewGrid(pointsEl.value);
+  });
 }
 
 // Main program
 window.onload = () => {
-    makeGrid(DEFAULT_SIZE);
-    activateButtons();
+  makeGrid(DEFAULT_SIZE);
+  activateButtons();
 };
