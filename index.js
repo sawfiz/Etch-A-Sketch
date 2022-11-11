24e4629ec64dbdea0bebacd0bbd7b0fad71907aa
sawfiz <dashu.up@gmail.com>
2022-11-11 15:48:28 +0800

    Refactor the js code into modules

--- index.js
+++ index.js
@@ -1,82 +1,30 @@
 const DEFAULT_SIZE = 45;
-let drawing = false;
-let mode = 'black';
-let color = '#000000';
-let personalColor = color;
-let currentSize = DEFAULT_SIZE;
 
-const gridContainerEl = document.querySelector('.grid-container');
-const btnColorEl = document.querySelector('#btn-color');
-const btnRainbowEl = document.querySelector('#btn-rainbow');
-const btnEraseEl = document.querySelector('#btn-erase');
-const btnClearEl = document.querySelector('#btn-clear');
-const pointsEl = document.querySelector('#points');
-const labelEl = document.querySelector('label');
-const colorPickerEl = document.querySelector('#color-picker');
+// Click handlers
+const clicksHandler = (() => {
+  const btnColorEl = document.querySelector('#btn-color');
+  const btnRainbowEl = document.querySelector('#btn-rainbow');
+  const btnEraseEl = document.querySelector('#btn-erase');
+  const btnClearEl = document.querySelector('#btn-clear');
+  const pointsEl = document.querySelector('#points');
+  const labelEl = document.querySelector('label');
+  const colorPickerEl = document.querySelector('#color-picker');
 
-const modalEl = document.querySelector('.modal');
-const cancelBtn = document.querySelector('#cancel-btn');
-const confirmBtn = document.querySelector('#confirm-btn');
+  const modalEl = document.querySelector('.modal');
+  const cancelBtn = document.querySelector('#cancel-btn');
+  const confirmBtn = document.querySelector('#confirm-btn');
 
-function getRandomColor() {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
+  let mode = 'black';
+  let color = '#000000';
+  let personalColor = color;
 
-function changeColor(dot) {
-  if (mode === 'rainbow') {
-    color = getRandomColor();
+  // Clear status of all buttons
+  function removeActive() {
+    btnEraseEl.classList.remove('active');
+    btnColorEl.classList.remove('active');
+    btnRainbowEl.classList.remove('active');
   }
-  dot.style.backgroundColor = color;
-}
 
-function drawStuff() {
-  if (drawing === true) {
-    changeColor(this);
-  }
-}
-
-// Make a new grid and add an eventlistener to each item in the grid
-function makeGrid(dim) {
-  gridContainerEl.style.cssText = `grid-template-columns: repeat(${dim}, 1fr); 
-        grid-template-rows: repeat(${dim}, 1fr)`;
-
-  // eslint-disable-next-line no-plusplus
-  for (let row = 0; row < dim; row++) {
-    // eslint-disable-next-line no-plusplus
-    for (let col = 0; col < dim; col++) {
-      const dotEl = document.createElement('item');
-      gridContainerEl.appendChild(dotEl);
-      dotEl.addEventListener('mouseover', drawStuff);
-      dotEl.addEventListener('mousedown', () => {
-        drawing = true;
-        changeColor(dotEl);
-      });
-      dotEl.addEventListener('mouseup', () => {
-        drawing = false;
-      });
-    }
-  }
-}
-
-function emptyGrid() {
-  gridContainerEl.innerHTML = '';
-}
-
-function makeNewGrid(dim) {
-  emptyGrid();
-  makeGrid(dim);
-  currentSize = dim;
-}
-
-// Clear status of all buttons
-function removeActive() {
-  btnEraseEl.classList.remove('active');
-  btnColorEl.classList.remove('active');
-  btnRainbowEl.classList.remove('active');
-}
-
-// Activate the event listeners
-function activateButtons() {
   // Start with the single color button active, default to black
   btnColorEl.classList.add('active');
   btnColorEl.addEventListener('click', () => {
@@ -111,14 +59,14 @@ function activateButtons() {
     btnEraseEl.classList.add('active');
   });
 
-  // Clear the grid
-  btnClearEl.addEventListener('click', () => {
-    modalEl.showModal();
-  });
-
   // Dragging on grid size updates the label
   pointsEl.addEventListener('input', () => {
     labelEl.innerText = `${pointsEl.value} x ${pointsEl.value} `;
+  });
+
+  // Clear the grid
+  btnClearEl.addEventListener('click', () => {
+    modalEl.showModal();
   });
 
   // Change grid size brings up a confirmation modal
@@ -126,25 +74,95 @@ function activateButtons() {
     labelEl.innerText = `${pointsEl.value} x ${pointsEl.value} `;
     modalEl.showModal();
   });
-}
+
+  cancelBtn.addEventListener('click', () => {
+    modalEl.close();
+    const size = canvasManager.getCurrentSize();
+    labelEl.innerText = `${size} x ${size} `;
+    pointsEl.value = size;
+  });
+
+  confirmBtn.addEventListener('click', () => {
+    if (mode === 'erase') {
+      mode = 'personal';
+      color = personalColor;
+      removeActive();
+      btnColorEl.classList.add('active');
+    }
+    canvasManager.makeNewGrid(pointsEl.value);
+    modalEl.close();
+  });
+
+  const getColor = () => color;
+
+  const getMode = () => mode;
+
+  return { getColor, getMode };
+})();
+
+// Object that manages the canvas
+const canvasManager = (() => {
+  let currentSize = DEFAULT_SIZE;
+  let drawing = false;
+  const gridContainerEl = document.querySelector('.grid-container');
+
+  function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  function changeColor(dot) {
+    if (clicksHandler.getMode() === 'rainbow') {
+      dot.style.backgroundColor = getRandomColor();
+    } else {
+      dot.style.backgroundColor = clicksHandler.getColor();
+    }
+  }
+
+  function drawStuff() {
+    if (drawing === true) {
+      changeColor(this);
+    }
+  }
+
+  // Make a new grid and add an eventlistener to each item in the grid
+  function makeGrid(dim) {
+    gridContainerEl.style.cssText = `grid-template-columns: repeat(${dim}, 1fr); 
+        grid-template-rows: repeat(${dim}, 1fr)`;
+
+    // eslint-disable-next-line no-plusplus
+    for (let row = 0; row < dim; row++) {
+      // eslint-disable-next-line no-plusplus
+      for (let col = 0; col < dim; col++) {
+        const dotEl = document.createElement('item');
+        gridContainerEl.appendChild(dotEl);
+        dotEl.addEventListener('mouseover', drawStuff);
+        dotEl.addEventListener('mousedown', () => {
+          drawing = true;
+          changeColor(dotEl);
+        });
+        dotEl.addEventListener('mouseup', () => {
+          drawing = false;
+        });
+      }
+    }
+  }
+
+  function emptyGrid() {
+    gridContainerEl.innerHTML = '';
+  }
+
+  function makeNewGrid(dim) {
+    emptyGrid();
+    makeGrid(dim);
+    currentSize = dim;
+  }
+
+  const getCurrentSize = () => currentSize;
+
+  return { makeNewGrid, makeGrid, getCurrentSize };
+})();
 
 // Main program
 window.onload = () => {
-  makeGrid(DEFAULT_SIZE);
-  activateButtons();
+  canvasManager.makeGrid(DEFAULT_SIZE);
 };
-
-cancelBtn.addEventListener('click', () => {
-  modalEl.close();
-});
-
-confirmBtn.addEventListener('click', () => {
-  if (mode === 'erase') {
-    mode = 'personal';
-    color = personalColor;
-    removeActive();
-    btnColorEl.classList.add('active');
-  }
-  makeNewGrid(pointsEl.value);
-  modalEl.close();
-});
